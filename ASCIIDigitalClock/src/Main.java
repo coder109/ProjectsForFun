821b7406f69dab5376ec0ab2c7c34b339cab7fe6
coder109 <3211697682@qq.com>
2025-01-31 17:49:21 +0800

    Update ASCII Digital Clock

---
+++ ASCIIDigitalClock/src/Main.java
@@ -0,0 +1,10 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+import com.Clock.Clock;
+
+public class Main {
+    public static void main(String[] args) {
+        Clock clock = new Clock();
+        clock.showTime();
+    }
+}
