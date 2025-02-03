6763b1138eb9a038a442e0c6b2df6b380d66d124
coder109 <3211697682@qq.com>
2025-02-03 20:01:34 +0800

    Fix Hanoi: Remove useless vars

--- Hanoi/hanoi.scm
+++ Hanoi/hanoi.scm
@@ -1,8 +1,6 @@
 ; Plates are 1 ~ nPlate
 (define nPlate 5)
 
-(define lResult (list))
-
 (define (GenerateResult idx from to)
     (printf 
         (string-append (number->string idx) ": " (number->string from) "->" (number->string to) "\n")
