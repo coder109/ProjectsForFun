7783fb07b974f71e58fdb9468b568d54641f7a31
Coder109 <3211697682@qq.com>
2025-03-25 15:31:46 +0800

    Update ToyTransformers: Too busy to work on this~

--- ToyTransformer/input.py
+++ ToyTransformer/input.py
@@ -12,3 +12,18 @@ class InputGenerator():
         for i in range(1, self.seq_len):
             output[:, i] = output[:, i-1] + 1
         return output
+    
+class Trainer():
+    def __init__(self,
+                 lr=1e-3,
+                 epochs=10,
+                 batch_size=64,
+                 seq_len=d_model,
+                 ):
+        self.lr = lr
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.seq_len = seq_len
+
+    def train(self, model, optimizer, criterion, train_loader, val_loader):
+        for epoch in range(self.epochs):
