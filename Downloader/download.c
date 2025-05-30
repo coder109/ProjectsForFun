84bb44e8e803a9ef48eba53eb8b1e38817ef4e5e
Coder109 <3211697682@qq.com>
2025-05-30 14:03:09 +0800

    Update Linux Downloader: HTTP Only.

--- Downloader/download.c
+++ Downloader/download.c
@@ -39,6 +39,27 @@ void win_download(const char* url, const char* filename) {
 }
 #elif __linux__
 void linux_download(const char *url, const char *filename) {
+    // Parse the url, remove http/https.
+    char *parsed_url = strstr(url, "://");
+    if(parsed_url != NULL) {
+        parsed_url = parsed_url + 3;
+    }
+
+    // Get pure domain
+    char* pure_domain = (char*)calloc(strlen(parsed_url) + 1, sizeof(char));
+    for(int i = 0; i < strlen(parsed_url); i++) {
+        if(parsed_url[i] == '/') {
+            break;
+        }
+        pure_domain[i] = parsed_url[i];
+    }
+
+    char* file_name = (char*)calloc(strlen(parsed_url) + 1, sizeof(char));
+    file_name = strstr(parsed_url, "/");
+    if(file_name != NULL) {
+        file_name = file_name + 1;
+    }
+
     // Create Socket
     int main_socket = socket(AF_INET, SOCK_STREAM, 0);
     if(main_socket < 0) {
@@ -46,11 +67,60 @@ void linux_download(const char *url, const char *filename) {
         return;
     }
 
-    // Setup the server address
+    // Lookup the IP Address
+    struct hostent *server = gethostbyname(pure_domain);
+    if(server == NULL) {
+        perror("IP Address Lookup Failed");
+        return;
+    }
+
+    // Fill in the structure
     struct sockaddr_in server_address;
     memset(&server_address, 0, sizeof(server_address));
     server_address.sin_family = AF_INET;
     server_address.sin_port = htons(80);
-    server_address.sin_addr.s_addr = inet_addr("127.0.0.1");
+    memcpy(&server_address.sin_addr.s_addr, server->h_addr_list[0], server->h_length);
+
+    // Try to connect with the server
+    if(connect(main_socket, (struct sockaddr*)&server_address, sizeof(server_address)) < 0) {
+        perror("Connection Failed");
+        return;
+    } else {
+        printf("Connected\n");
+    }
+
+    // Send the request
+    char request[1024];
+    sprintf(request, "GET /%s HTTP/1.1\r\nHost: %s:80\r\n\r\n", file_name, pure_domain);
+    if(send(main_socket, request, strlen(request), 0) < 0) {
+        perror("Request Failed");
+        return;
+    } else {
+        printf("Request Sent\n");
+    }
+
+    // Prepare the buffer
+    char buffer[BUFFER_SIZE] = {0};
+
+    // Download
+    int bytes_read = 0;
+    FILE *fp = fopen(filename, "wb");
+    printf("Downloading...\n");
+    do {
+        bytes_read = recv(main_socket, buffer, BUFFER_SIZE, 0);
+        if(bytes_read > 0) {
+            printf("Downloaded %d bytes\n", bytes_read);
+            fwrite(buffer, sizeof(char), bytes_read, fp);
+        } else if(bytes_read < 0) {
+            perror("Download Failed");
+            break;
+        } else {
+            break;
+        }
+    } while(bytes_read > 0);
+
+    // Finish
+    fclose(fp);
+    close(main_socket);
 }
 #endif
